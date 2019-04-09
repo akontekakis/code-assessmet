a2df25d7a2bc7094f9bafa7933b287b75b1561b1
Adrian Kontekakis <adrian.kontekakis@uon.edu>
2019-04-09 03:26:18 +0000

    currently im at the stage of trying to allocate the message to ASCII values

---
+++ main.c
@@ -0,0 +1,48 @@
+//Caeser cipher
+char_message(m)
+
+
+int main ()
+{
+char let
+// let is the letter that will be printed after encryption
+int key, i; 
+//values above are the key of encryption, and i is an int to use in for loops
+
+printf("enter a messsage:");
+
+for(x=0 ; message[80] != '\0' ; i++)
+{
+    scanf("%c", &m);
+    
+    
+    
+    
+}
+message[i] = m;
+
+
+
+char message[80];
+
+printf("enter the key:")
+
+scanf("%d," &key);
+
+for (int i=0; message[0] != '\0';i++)
+
+char_message(m)
+{
+    char c = 'B';
+int temp;
+temp = c;
+int ascii[80];
+    
+    
+    
+let = (m + key)(%26);
+
+printf("%c," let);
+
+}
+}
