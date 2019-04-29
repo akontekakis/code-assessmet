a95ca5f5f4b8e93a1fd41815aeafbdd00c3e8ec2
Adrian Kontekakis <adrian.kontekakis@uon.edu>
2019-04-29 02:19:46 +0000

    Everything is finished and how it is meant to be

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
 int main ()
 {
-int encryption_type = 3;    //this integer is used in the switch statement to select whether 
+int encryption_type = 4;    //this integer is used in the switch statement to select whether 
 //rotation or substitution encrytion or decryption is used.
 // 1 for rotational encryption, 2 for rotational decryption 
 // 3 for substitution encryption, 4 for substitution decryption
@@ -16,7 +16,7 @@ case 1:
         int key = 2;        //the value that determines the value of rotation
         int i;              // is an int to use in for loops
  
-        for(i=0 ; message[i] != '\0' ; i++)  //the for loop makes the code in the loop apply to each letter in the array, this encrypts each letter and then saves it a
+        for(i=0 ; message[i] != '\0' ; i++)  //the for loop makes the code in the loop apply to each letter in the array, this encrypts each letter and then saves it as an array
 { 
             if (message[i] >96 && message[i] <123)  // if the ASCII characters are between these values they represent lower case letters, this means the below code will run 
 {
@@ -42,7 +42,7 @@ case 2:         // rotational decryption
         int key = 2;        //the value that determines the value of rotation
         int i;              // is an int to use in the  for loops
  
-        for(i=0 ; message[i] != '\0' ; i++)
+        for(i=0 ; message[i] != '\0' ; i++)     //the for loop makes the code in the loop apply to each letter in the array, this encrypts each letter and then saves it as an array
 { 
             if (message[i] >96 && message[i] <123)      // if the ASCII characters are between these values they represent lower case letters, this means the below code will run
 {
@@ -60,17 +60,17 @@ case 2:         // rotational decryption
     printf("%s", message);   // it prints the array to the screen
 
 
-    break;
+    break;      //this just stops the code here 
 }
     
  case 3:            // substitution encryption
  {
-    char message[80] = {"PlEASe make this work you fucking che"};      // initialise the string with a message to be encrypted
+    char message[80] = {"Please work, please work"};      // initialise the string with a message to be encrypted
     char key[26]= {"WHPLCOXDEJZUFITQKRYGVSNMBA"};     //rearange the alphabet to form the key, this means that A is assigned to the first letter, B to the second and so on
     int i;   // is an int to use in for loops
     int x=0; //x is used to represent a single part of the array that is the key
     
-    for(i=0 ; message[i] != '\0' ; i++)
+    for(i=0 ; message[i] != '\0' ; i++)     //the for loop makes the code in the loop apply to each letter in the array, this encrypts each letter and then saves it as an array
     {
          if (message[i] >96 && message[i] <123) // if the ASCII characters are between these values they represent lower case letters, this means the below code will run
 {
@@ -78,9 +78,9 @@ case 2:         // rotational decryption
 }
     if (message[i] > 64 && message[i] <91)      // if all characters are captial letters (the ASCII characters that represent capital letters) the below code will run
 {
-    message[i] = message[i] - 65; 
-    x= message[i];
-    message[i]= key[x];
+    message[i] = message[i] - 65;     // this gets it out of ASCII characters to the positions 0-25 or A-Z
+    x= message[i];                    // this assigns the 0-25 character assigned to an integer of the alphabet
+    message[i]= key[x];         //the integer determines how far down the alphabet the letter is assigned         
 
 } 
     }
@@ -95,12 +95,12 @@ case 2:         // rotational decryption
 
 case 4:         //substitution decryption
     {
-    char message[80] = {"QUCWYC FWZC GDEY NTRZ BTV OVPZEIX PDC"}; // initialise the string with a message to be encrypted
+    char message[80] = {"QUCWYC NTRZ, QUCWYC NTRZ"}; // initialise the string with a message to be encrypted
     char key[26]= {"WHPLCOXDEJZUFITQKRYGVSNMBA"};     //rearange the alphabet to form the key, this means that A is assigned to the first letter, B to the second and so on
     int i;      // is an int to use in for loops
     int x=0;    //x is used to represent a single part of the array that is the key
     
-    for(i=0 ; message[i] != 0 ; i++)
+    for(i=0 ; message[i] != 0 ; i++)        //the for loop makes the code in the loop apply to each letter in the array, this encrypts each letter and then saves it as an array
     {
          if (message[i] >96 && message[i] <123) // if the ASCII characters are between these values they represent lower case letters, this means the below code will run
 {
@@ -108,25 +108,24 @@ case 4:         //substitution decryption
 }
     if (message[i] > 64 && message[i] <91)       // if all characters are captial letters (the ASCII characters that represent capital letters) the below code will 
 {
-    x = 0;
-    while (message[i] != key[x])
+    x = 0;                              //this declares x as 0 
+    while (message[i] != key[x])        // this checks if the message is equal to the value of x, if it isnt 1 is added 
     {
-        x = x+1;
+        x = x+1;        //this adds one to x
 }
-x=x+65;
-message[i]=x;
+x=x+65;         // add 65 to x so it becomes an ASCII character
+message[i]=x;      // it assigns the ASCII character to the message giving it a letter 
 }
-printf("%c", message[i]); // this prints each letter of the encrypted message as it is encrypted
+printf("%c", message[i]); // this prints each letter of the encrypted message as it is encrypted each time it goes through the for loop
 }
 break; // this stops the code meaning it doesnt continue through all the other parts of the swtitch statements
 }
 
 
-default: 
+default: // the code jumps to this spot if 1,2,3 or 4 are not declared in encryption_type, telling the user that they have made a mistake
 {
-printf("error in chosing an ecryption / decryption type");
+printf("error in chosing an ecryption / decryption type"); // this just prints the message informing the user of their error
 }
 
- 
 }
 }
